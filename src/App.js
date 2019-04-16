b6eceac5347a3ba372de6a86032faac3eb7544d7
briantical <lutbrianivan@gmail.com>
2019-04-16 19:41:06 +0300

    Adding images and styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import './styles.css';
+import './slider-animations.css';
 import Slider from 'react-animated-slider';
 import 'react-animated-slider/build/horizontal.css';
 
@@ -16,10 +18,13 @@ class App extends Component {
     this.getData();
   }
   getData = ()=>{
-    fetch(`https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=${process.env.REACT_APP_API_KEY}`)
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_API_KEY}`
+    console.log(url);
+    fetch(url)
     .then((res) =>res.json())
     .then(data=>{      
-      this.setState({data : data.results})      
+      this.setState({data : data.articles});
+      console.log(data.articles)     
       })
     .catch((error)=>console.log("Error:" + error))
   }
@@ -27,18 +32,29 @@ class App extends Component {
   render() {
     const {data} = this.state;
     return (
-      <div className="App">        
-        <Slider>          
+      <div className="wrapper">        
+        <Slider className="slider-wrapper">          
           {
             (data !== null) ? 
-            data.map((article, index) => 
-              <div key={index}>
-                <h2>{article.des_facet[0]}</h2>
-                <div>{article.url}</div>
+            data.map((article, index) =>
+              <div
+                key={index}
+                className="slider-content"
+                style={{ background: `url('${article.urlToImage}') no-repeat center center` }}
+              >   
+                <div className="inner">
+                  <h2>{article.title}</h2>
+                  <div>{article.description}</div>
+                </div>
+                <section>                  
+                  <span>
+                    Posted by <strong>{article.source.name}</strong>
+                  </span>
+                </section>                           
               </div>)
               :
               <h2>NOTHING YET</h2>
-          }
+          }          
         </Slider>
       </div>
     );
